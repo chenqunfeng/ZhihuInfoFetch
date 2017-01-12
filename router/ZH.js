cf12a7d318e6fe1cc63186030481e7af02c35c82
e <604590172@qq.com>
2017-01-12 17:52:48 +0800

    Add logs record

--- router/ZH.js
+++ router/ZH.js
@@ -1,35 +1,45 @@
 var express = require('express'),
     router = express.Router(),
-    ZH = require('./../component/ZH.js')
+    ZH = require('./../component/ZH.js'),
+    getIP = require('ipware')().get_ip,
+    log4js = require('log4js')
     ;
 
-var zh = new ZH();
+log4js.loadAppender('file');
+log4js.addAppender(log4js.appenders.file('./logs/info.log'), 'ZHFETCH');
+
+var zh = new ZH(),
+    logger = log4js.getLogger('ZHFETCH');
 
 router.get('/', function(req, res, next) {
 
+    logger.info(getIP(req))
+
     zh.verify(function(data) {
-        if ('logout' == data.status) {
-            _xsrf = data._xsrf
-            captcha = data.captcha
-            captcha_cn = data.captcha_cn
-            html = ""
-            html += "<html>"
-            html += "<head></head>"
-            html += "<body>"
-            html += "<div>" + _xsrf + "</div>"
-            html += "<img src='" + captcha + "'>"
-            html += "</body>"
-            html += "</html>"
-            return res.send(html)
-        } else {
-            return res.json(data)
-        }
-        
+        // if ('logout' == data.status) {
+        //     _xsrf = data._xsrf
+        //     captcha = data.captcha
+        //     captcha_cn = data.captcha_cn
+        //     html = ""
+        //     html += "<html>"
+        //     html += "<head></head>"
+        //     html += "<body>"
+        //     html += "<div>" + _xsrf + "</div>"
+        //     html += "<img src='" + captcha + "'>"
+        //     html += "</body>"
+        //     html += "</html>"
+        //     return res.send(html)
+        // } else {
+        //     return res.json(data)
+        // }
+        return res.json(data);
     })
 
 })
 
 router.post('/login', function(req, res, next) {
+
+    logger.info(getIP(req))
 
     params = req.body
 
@@ -40,6 +50,8 @@ router.post('/login', function(req, res, next) {
 })
 
 router.post('/getList', function(req, res, next) {
+
+    logger.info(getIP(req))
 
     params = req.body
 
